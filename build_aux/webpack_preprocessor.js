c2ef82129edb6836756765da7781b28730f6be08
Kinten Le <kinten108101@protonmail.com>
2026-05-31 13:28:22 +0700

    fix

--- build_aux/webpack_preprocessor.js
+++ build_aux/webpack_preprocessor.js
@@ -12,7 +12,7 @@ const firebase_key = (() => {
 	if (process.env.BKHACK_FIREBASE_KEY === undefined || typeof process.env.BKHACK_FIREBASE_KEY !== "string") {
 		throw new Error("did not specify BKHACK_FIREBASE_KEY")
 	}
-	return process.env.BKHACK_BACKEND_ADDRESS;
+	return process.env.BKHACK_FIREBASE_KEY;
 })()
 
 module.exports = {
