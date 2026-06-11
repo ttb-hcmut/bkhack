ac682e668d589e201ff2e118c6792cb0b1b9596c
Kinten Le <kinten108101@protonmail.com>
2026-06-11 18:19:09 +0700

    prettified elixir code

--- src/Service/lib/comment.ex
+++ src/Service/lib/comment.ex
@@ -58,142 +58,59 @@ defmodule DiscussionBE do
 		end end)
   end
 
-  defp discussionQuery(parent_id,parent_type\\"post",user_id\\nil) do
-    query = case parent_type do
-      "post"    -> from(c in Comment) |> where([c],c.parent_post_id == ^parent_id)
-      "comment" -> from(c in Comment) |> where([c],c.parent_comment_id == ^parent_id)
-    end
-    comment_sum_ratings =
-      from(c in subquery(query|> select([c],%{comment_id: c.comment_id})))
-      |> join(:left,[c], r in CommentRating, on: c.comment_id == r.comment_id)
-      |> select([c,r], %{
-        comment_id: c.comment_id,
-        commentsumratings: coalesce(sum(r.rating),0),
-        })
-      |> group_by([c,r], c.comment_id)
+	defp discussionQuery(parent_id,parent_type\\"post",user_id\\nil)
+		do
 
-    comment_rating =
-      from(c in subquery(query|> select([c],%{comment_id: c.comment_id})))
-      |> join(:left,[c], r in CommentRating, on: c.comment_id == r.comment_id)
-      |> select([c,r], %{
-        comment_id: c.comment_id,
-        rating:     r.rating,})
-      |> then(fn x -> case is_nil(user_id) do
-        true ->  x |> where([c,r], r.voter_id != r.voter_id)
-        false -> x |> where([c,r], r.voter_id == ^user_id)
-      end end)
+		query = case parent_type do
+			"post"    -> from(c in Comment) |> where([c],c.parent_post_id == ^parent_id)
+			"comment" -> from(c in Comment) |> where([c],c.parent_comment_id == ^parent_id)
+		end
 
-    full_query =
-      from(c in subquery(query|> select([c],c)))
-      |> join(:left,[c], r in subquery(comment_rating), on: c.comment_id == r.comment_id)
-      |> join(:left,[c,r], sr in subquery(comment_sum_ratings), on: c.comment_id == sr.comment_id)
-      |> join(:left,[c,r,sr], u in User, on: c.commenter_id == u.user_id)
-      |> select([c,r,sr,u], %{
-        id:           c.comment_id,
-        text:         c.content,
-        user_rating:  coalesce(r.rating,0),
-        rating:       sr.commentsumratings,
-        timestamp:    c.date_created_utc,
-        post_vers:    c.post_version,
-        author_name:  u.name,
-        author_id:    u.user_id,
-        author_role:  u.role })
-    full_query
-  end
+		comment_sum_ratings =
+			from(c in subquery(query|> select([c],%{comment_id: c.comment_id})))
+			|> join(:left,[c], r in CommentRating, on: c.comment_id == r.comment_id)
+			|> select([c,r], %{
+				comment_id: c.comment_id,
+				commentsumratings: coalesce(sum(r.rating),0),
+				})
+			|> group_by([c,r], c.comment_id)
 
-   # def fromCommentTree(parent_id\\-1,parent_type\\"post",opts\\%{}) do
-  #   base_query =
-  #     from(c in Comment)
-  #     |> select([c], c)
-  #     |> then(fn x -> case {parent_id,parent_type} do
-  #       {-1,_}        -> x
-  #       {_,"post"}    -> x |> where([c], c.parent_post_id    == ^parent_id)
-  #       {_,"comment"} -> x |> where([c], c.parent_comment_id == ^parent_id)
-  #       {_,_}         -> x
-  #     end end)
-  #   recursive_query =
-  #     from(c in Comment)
-  #     |> join(:inner, [c], ct in "comment_tree", on: ct.comment_id == c.parent_comment_id)
-  #     |> select([c, ct], c)
-  #   comment_tree =
-  #     base_query
-  #     |> union(^recursive_query)
+		comment_rating =
+			from(c in subquery(query|> select([c],%{comment_id: c.comment_id})))
+			|> join(:left,[c], r in CommentRating, on: c.comment_id == r.comment_id)
+			|> select([c,r], %{
+				comment_id: c.comment_id,
+				rating:     r.rating,})
+			|> then(fn x -> case is_nil(user_id) do
+				true ->  x |> where([c,r], r.voter_id != r.voter_id)
+				false -> x |> where([c,r], r.voter_id == ^user_id)
+			end end)
 
-  #   from(ct in "comment_tree") |> recursive_ctes(true) |> with_cte("comment_tree", as: ^comment_tree)
-  # end
+		full_query =
+			from(c in subquery(query|> select([c],c)))
+			|> join(:left,[c], r in subquery(comment_rating), on: c.comment_id == r.comment_id)
+			|> join(:left,[c,r], sr in subquery(comment_sum_ratings), on: c.comment_id == sr.comment_id)
+			|> join(:left,[c,r,sr], u in User, on: c.commenter_id == u.user_id)
+			|> select([c,r,sr,u], %{
+				id:           c.comment_id,
+				text:         c.content,
+				user_rating:  coalesce(r.rating,0),
+				rating:       sr.commentsumratings,
+				timestamp:    c.date_created_utc,
+				post_vers:    c.post_version,
+				author_name:  u.name,
+				author_id:    u.user_id,
+				author_role:  u.role })
 
-  # def getPostComments(user_id\\0,post_id\\0,offset\\0,limit\\10) do
-  #   query = """
-  #     WITH commentsumratings as (
-  #       SELECT comment_id, SUM(CASE WHEN rating = 1 THEN 1 ELSE -1 END) as total
-  #       FROM commentratings
-  #       GROUP BY comment_id
-  #     )
-  #     SELECT
-  #       c.comment_id as id,
-  #       c.content as text,
-  #       SUM(CASE WHEN r.voter_id = ? THEN r.rating ELSE 0 END) as user_rating,
-  #       CASE WHEN s.total IS NOT NULL then s.total else 0 end as rating,
-  #       c.date_created_utc as timestamp,
-  #       c.post_version as post_vers,
-  #       u.name as author_name,
-  #       u.user_id as author_id,
-  #       u.role as author_role
-  #     FROM comments c
-  #     LEFT JOIN commentratings r on c.comment_id = r.comment_id
-  #     LEFT JOIN commentsumratings s on c.comment_id = s.comment_id
-  #     LEFT JOIN users u on c.commenter_id = u.user_id
-  #     where c.parent_post_id = ?
-  #     group by id
-  #     ORDER BY id
-  #     LIMIT ?
-  #     OFFSET ?
-  #   """
-  #   {:ok,result} = Ecto.Adapters.SQL.query(data, query,[user_id,post_id,limit,offset])
-  #   columns = Enum.map(result.columns, fn c -> String.to_atom(c) end)
-  #   result.rows |> Enum.map(fn row -> Enum.zip(columns, row) end)
-  # end
-
-  # def getCommentReplies(user_id\\0,comment_id\\0,offset\\0,limit\\10) do
-  #   query = """
-  #     WITH commentsumratings as (
-  #       SELECT comment_id, SUM(CASE WHEN rating = 1 THEN 1 ELSE -1 END) as total
-  #       FROM commentratings
-  #       GROUP BY comment_id
-  #     )
-  #     SELECT
-  #       c.comment_id as id,
-  #       c.content as text,
-  #       SUM(CASE WHEN r.voter_id = ? THEN r.rating ELSE 0 END) as user_rating,
-  #       CASE WHEN s.total IS NOT NULL then s.total else 0 end as rating,
-  #       c.date_created_utc as timestamp,
-  #       c.post_version as post_vers,
-  #       u.name as author_name,
-  #       u.user_id as author_id,
-  #       u.role as author_role
-  #     FROM comments c
-  #     LEFT JOIN commentratings r on c.comment_id = r.comment_id
-  #     LEFT JOIN commentsumratings s on c.comment_id = s.comment_id
-  #     LEFT JOIN users u on c.commenter_id = u.user_id
-  #     where c.parent_comment_id = ?
-  #     group by id
-  #     ORDER BY id
-  #     LIMIT ?
-  #     OFFSET ?
-  #   """
-  #   {:ok,result} = Ecto.Adapters.SQL.query(data, query,[user_id,comment_id,limit,offset])
-  #   columns = Enum.map(result.columns, fn c -> String.to_atom(c) end)
-  #   result.rows |> Enum.map(fn row -> Enum.zip(columns, row) end)
-  # end
+		full_query
+		end
 
   def setVote(data, voter_id,comment_id,action) do
     from(r in CommentRating, where: r.comment_id == ^comment_id and r.voter_id == ^voter_id )
     |> data.delete_all()
-
     if action == -1 || action == 1 do
       data.insert!(%CommentRating{voter_id: voter_id, comment_id: comment_id, rating: action})
     end
-
   end
 
   def postComment(data, parent_id,parent_type,content,commenter_id,post_version) do
